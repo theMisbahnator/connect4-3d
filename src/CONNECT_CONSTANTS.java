dcfd3223a0adab4b9671df1cd3b55b15217cd9d9
Misbah <misb2001@gmail.com>
2021-05-21 02:35:03 -0500

    comments for the constants class

--- src/CONNECT_CONSTANTS.java
+++ src/CONNECT_CONSTANTS.java
@@ -1,15 +1,14 @@
+/**
+ * Constants used throughout this project
+ */
 public interface CONNECT_CONSTANTS {
 
+    // The length and width of each 2-D plane
     public static final int PLANE_SIZE = 5;
 
-    public static final int MID_INDEX = 2;
+    // The mid section of a 2-d plane
+    public static final int MID_INDEX = PLANE_SIZE / 2;
 
+    // The height of the planes with zero based indexing
     public static final int HEIGHT = 3;
-
-
-
-
-
-
-
 }
