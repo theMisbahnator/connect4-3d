637c942d63f1f5e64fc5c276ada7d0c79d9963de
Misbah <misb2001@gmail.com>
2021-05-21 03:56:23 -0500

    Coding hygiene --- Added comments!

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 import java.util.Iterator;
-
 /**
- * The playing board that holds the connect 4 pieces.
- * The structure is a cube with PLANE_SIZE amount of
+ * The Game board that holds the pieces.
+ * The structure is a cube with PLANE_SIZE (4) amount of
  * Planes. When inserting a piece into the board
  * given a row and column, the piece slots down
  * to the lowest available position within the cube.
@@ -24,9 +23,9 @@ public class Board implements CONNECT_CONSTANTS {
     }
 
     /**
-     * FOR TESTING PURPOSES: Takes in 4 already filled
-     * layers and creates a valid board. Assume
-     * the board is correct.
+     * FOR TESTING PURPOSES ONLY: Takes in 4 already filled
+     * planes and creates a board. Assume
+     * the board's construction is correct.
      *
      * @param layer0 the first layer of the board
      * @param layer1 the second layer of the board
@@ -34,6 +33,11 @@ public class Board implements CONNECT_CONSTANTS {
      * @param layer3 the fourth layer of the board
      */
     public Board(int[][] layer0, int[][] layer1, int[][] layer2, int[][] layer3) {
+        if (layer0 == null || layer1 == null || layer2 == null || layer3 == null) {
+            throw new IllegalArgumentException("Violation: Parameters for the " +
+                    "board tester constructor are null.");
+        }
+        // tests only optimized for a board with 4 layers
         CONNECT_BOARD = new Plane[HEIGHT + 1];
         CONNECT_BOARD[0] = new Plane(layer0);
         CONNECT_BOARD[1] = new Plane(layer1);
@@ -63,24 +67,45 @@ public class Board implements CONNECT_CONSTANTS {
      * Gets a square within the Board given
      * a row, col, and height. i.e (x, y, z).
      *
-     * @param row row position to place desired piece
-     * @param col column position to place desired
-     * @return true if the given row and column pos isn't
-     * filled.
+     * @param row row index for desired square
+     * @param col column index for desired square
+     * @param height height index for desired square
+     * @return a square containing either an empty piece,
+     * or a square filled by a game piece.
      */
     public EmptySquare getSquare(int row, int col, int height) {
         return CONNECT_BOARD[height].getSquare(row, col);
     }
 
+    /**
+     * Gets the top of a stack given a row and col.
+     * if there are no pieces on a given coordinate,
+     * return null.
+     *
+     * @param row desired row index
+     * @param col desired column index
+     * @return a square with the top most piece or null
+     * if there are no pieces.
+     */
     public EmptySquare getOpenSquare(int row, int col) {
+        EmptySquare openSquare = null;
         for (Plane e : CONNECT_BOARD) {
             if (!e.getSquare(row, col).isEmpty()) {
-                return e.getSquare(row, col);
+                openSquare = e.getSquare(row, col);
             }
         }
-        return null;
+        return openSquare;
     }
 
+    /**
+     * Gets the height of the top most piece
+     * on a given position within the board.
+     *
+     * @param row desired row index
+     * @param col desired column index
+     * @return a int indicating the height of the
+     * top most piece within the desired spot.
+     */
     public int getOpenSquareHeight(int row, int col) {
         int height = 0;
         for (Plane e : CONNECT_BOARD) {
@@ -92,17 +117,65 @@ public class Board implements CONNECT_CONSTANTS {
         return height;
     }
 
-
-    public boolean isGameDone(int row, int col, int height, Piece Player) {
-        // diagonal check, four diagonals from a given piece
-        return CONNECT_BOARD[height].isGameDone(Player) || checkMultiDimensions(row, col, height, Player);
+    /**
+     * Adds a piece from a team to the board. Returns
+     * a boolean indicating if the piece added led to
+     * a connect four.
+     * pre: row and col must lead to a placement within
+     * the bard that is available.
+     *
+     * @param addedPiece piece added to the board
+     * @param row desired row index to add piece
+     * @param col desired col index to add piece
+     * @return true if the piece add leads to a connect four.
+     */
+    public boolean addUserPiece(Piece addedPiece, int row, int col) {
+        if (!isSquareAvailable(row, col)) {
+            throw new IllegalStateException("Square already taken.");
+        }
+        int height = getOpenSquareHeight(row, col);
+        CONNECT_BOARD[height].setSquare(row, col, addedPiece);
+        return isGameDone(row, col, height, addedPiece) || CONNECT_BOARD[height].isGameDone(addedPiece);
     }
 
+    /**
+     * Determines if the game is completed if there is a
+     * connect four based on the previous move made. This is
+     * the efficient version to determine a winner since it
+     * only examines the valid paths from the previous move
+     * versus examining all paths from a given team.
+     *
+     * @param row row index from the prev move
+     * @param col column index from the prev move
+     * @param height height index from the prev move
+     * @param Player the player who made the prev move
+     * @return true if the previous move created a connect 4.
+     */
+    public boolean isGameDone(int row, int col, int height, Piece Player) {
+        // checks both the 2-D plane and the valid diagonals in 3-D space
+        return CONNECT_BOARD[height].isGameDone(Player) ||
+                checkMultiDimensions(row, col, height, Player);
+    }
+
+    /**
+     * FOR TESTING PURPOSES ONLY: Determines if the game
+     * is completed if there is a connect based on checking
+     * all the moves from a given team. This is the less
+     * efficient method for determining a winner since it
+     * checks all moves made from a given player and not
+     * the previous one which is all that is needed.
+     *
+     * @param Player the piece type being used to check for a
+     *               connect four.
+     * @return true if the previous move created a connect 4.
+     */
     public boolean isGameDone(Piece Player) {
         for (int i = 0; i < CONNECT_BOARD.length; i++) {
+            // iterates through all squares in plane (25)
             for (int r = 0; r < PLANE_SIZE; r++) {
                 for (int c = 0; c < PLANE_SIZE; c++) {
                     EmptySquare s = CONNECT_BOARD[i].getSquare(r, c);
+                    // only initiates search algorithm on squares with same team
                     if (!s.isEmpty() && s.getTeam() == Player.getTeam()) {
                         if (isGameDone(r, c, i, Player)) {
                             return true;
@@ -114,55 +187,81 @@ public class Board implements CONNECT_CONSTANTS {
         return false;
     }
 
+    /**
+     * Assembles all valid paths needed to traverse through the board in
+     * order to find a connect four given a point. A path contains two parts:
+     * - The first half of a diagonal from a given point. Uses the normative
+     *   List of path created using the search look up table.
+     * - The second half of a diagonal from a given point. Uses the negated
+     *   version of a path that allows a mirrored search from the opposite
+     *   side of a given point.
+     *
+     * Each diagonal can range from size 0 to 5, yet given the programs
+     * recursive mechanism, the program stops forming a diagonal when
+     * the pieces are different from the desired or goes out of bounds.
+     * When there is a string of similar pieces that add up to 4, we
+     * can determine there is a winner.
+     *
+     * @param row row index of prev move
+     * @param col column index of prev move
+     * @param height height index of prev move
+     * @param player desired piece type when looking for winner
+     * @return true if there is a connection of for in 3-D space.
+     */
     private boolean checkMultiDimensions(int row, int col, int height, Piece player) {
+        // contains all valid search paths for a given point (x, y, z)
         SearchLookUpTable validPaths = new SearchLookUpTable(row, col, height);
         Iterator<Direction> paths = validPaths.iterator();
         while (paths.hasNext()) {
             Direction path = paths.next();
-            if (checkConnectFour(row, col, height, player, path)) {
+            // since prev point already counts, iterate over that
+            int diagonalOne = inARow(row + path.getRowROC(),
+                    col + path.getColROC(), height + path.getHeightROC(), player, path);
+            int diagonalTwo = inARow(row, col, height, player, path.getOtherDir());
+        if (diagonalOne + diagonalTwo >= THRESHOLD) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean checkConnectFour(int row, int col, int height, Piece player, Direction path) {
-        Direction inversePath = path.getOtherDir();
-        return inARow(row, col, height, player, inversePath) + inARow(row + path.getRowROC(),
-                col + path.getColROC(), height + path.getHeightROC(), player, path) >= 4;
-    }
-
+    /**
+     * Recursive search algorithm that iterated over pieces in 3-D space.
+     * Accumulates a sum for every similar piece connected to last piece
+     * placed. Direction of iteration is dictated by path object created
+     * by the search table.
+     *
+     * @param row current row index in recursive call
+     * @param col current column index in recursive call
+     * @param height current height index in recursive call
+     * @param player desired piece being checked for a connection of
+     *               similar pieces
+     * @param path the rate of change for row, col, and height dictating
+     *             the movement from a given point in the recursive call.
+     * @return an int for every similar piece connected to the last piece placed.
+     */
     private int inARow(int row, int col, int height, Piece player, Direction path) {
-        if (row < 0 || col < 0 || height < 0 || row >= PLANE_SIZE || col >= PLANE_SIZE || height > HEIGHT) {
+        // base case: piece goes out of bounds
+        if (row < 0 || col < 0 || height < 0 || row >= PLANE_SIZE || col >= PLANE_SIZE
+                || height > HEIGHT) {
             return 0;
         }
         EmptySquare square = CONNECT_BOARD[height].getSquare(row, col);
+        // base case: current piece in call is different from last piece placed
         if (square.isEmpty() || square.getTeam() != player.getTeam()) {
             return 0;
         }
-        return 1 + inARow(row + path.getRowROC(), col + path.getColROC(), height + path.getHeightROC(), player, path);
+        // piece is the same type as the desired piece and is connected, continue 3-D iteration
+        return 1 + inARow(row + path.getRowROC(), col + path.getColROC(),
+                height + path.getHeightROC(), player, path);
     }
 
-    private int checkHeight(int changeInHeight, int height, int row, int col, Piece player) {
-        if (height >= PLANE_SIZE || height < 0) {
-            return 0;
-        }
-        boolean sameTeam = !getSquare(row, col, height).isEmpty() &&
-                (player.getTeam() == getSquare(row, col, height).getTeam());
-        return !sameTeam ? 0 : 1 + checkHeight(changeInHeight,
-                height + changeInHeight, row, col, player);
-    }
-
-    public boolean addUserPiece(Piece addedPiece, int row, int col) {
-        if (!isSquareAvailable(row, col)) {
-            throw new IllegalStateException("Square already taken");
-        }
-        int height = getOpenSquareHeight(row, col);
-        CONNECT_BOARD[height].setSquare(row, col, addedPiece);
-        return isGameDone(row, col, height, addedPiece) || CONNECT_BOARD[height].isGameDone(addedPiece);
-    }
-
-    @Override
+    /**
+     * Prints out the game board. Prints out each plane in
+     * order from top down.
+     *
+     * @return a string presenting the game board in 2-D fashion.
+     */
     public String toString() {
         return "Board{" +
                 "CONNECT_BOARD=" + Arrays.toString(CONNECT_BOARD) +
--- src/CONNECT_CONSTANTS.java
+++ src/CONNECT_CONSTANTS.java
@@ -3,12 +3,19 @@
  */
 public interface CONNECT_CONSTANTS {
 
-    // The length and width of each 2-D plane
+    // the height of the planes with zero based indexing
+    public static final int HEIGHT = 3;
+
+    // the length and width of each 2-D plane
     public static final int PLANE_SIZE = 5;
 
-    // The mid section of a 2-d plane
+    // the mid section of a 2-d plane
     public static final int MID_INDEX = PLANE_SIZE / 2;
 
-    // The height of the planes with zero based indexing
-    public static final int HEIGHT = 3;
+    // Num in a row needed for a winner (in this case Connect four!)
+    public static final int THRESHOLD = 4;
+
+
+
+
 }
