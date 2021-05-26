936de344731d90390dcd0bd5dfd37dbd5bab1fc3
Misbah <misb2001@gmail.com>
2021-05-26 01:05:23 -0500

    - Finished commenting and checking precons

--- src/SearchLookUpTable.java
+++ src/SearchLookUpTable.java
@@ -1,68 +1,116 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Objects;
 
 /**
  * Given a set of coordinates on a 3d plane (x, y, z),
  * this class returns all the valid search paths to check
  * for a connect 4. This limits unnecessary recursive calls
  * increasing efficiency.
- *
- * I used Minecraft to help visualize the coordinates'
- * valid paths and recorded the results on a google
- * sheets here:
  */
-public class SearchLookUpTable implements CONNECT_CONSTANTS{
+public class SearchLookUpTable implements CONNECT_CONSTANTS {
 
-    private ArrayList<Direction> validPaths;
+    private final ArrayList<Direction> VALID_PATHS;
 
+    /**
+     * Creates a bare set of valid paths used
+     * for testing purposes.
+     */
     public SearchLookUpTable() {
-        validPaths = new ArrayList<>();
+        VALID_PATHS = new ArrayList<>();
     }
 
+    /**
+     * Given a coordinate on a 3-D plane, there are
+     * 9 possible directions to traverse in 3-D space.
+     * However, traversing in certain directions are
+     * pointless if there isn't enough space to check
+     * four spaces. Therefore a set of possible paths
+     * from a point to create a connect 4 are assembled
+     * to reduce traversing in irrelevant directions.
+     *
+     * @param row    starting row index, 0 <= row < PLANE_SIZE
+     * @param col    starting column index, 0 <= col < PLANE_SIZE
+     * @param height starting height index, 0 <= height < HEIGHT
+     */
     public SearchLookUpTable(int row, int col, int height) {
         this();
-        if(col < MID_INDEX + height && col >= height) {
-            validPaths.add(new Direction(0,1, 1));
+        // precondition
+        if (row < 0 || col < 0 || height < 0 || row >= PLANE_SIZE || col >= PLANE_SIZE
+                || height > HEIGHT) {
+            throw new IllegalArgumentException("Violation: param when creating search table are " +
+                    "invalid.");
         }
+        // leftward diagonal
+        if (col < MID_INDEX + height && col >= height) {
+            VALID_PATHS.add(new Direction(0, 1, 1));
+        }
+        // rightward diagonal
         if (col > MID_INDEX - height && col < PLANE_SIZE - height) {
-            validPaths.add(new Direction(0,-1, 1));
+            VALID_PATHS.add(new Direction(0, -1, 1));
         }
+        // downward diagonal
         if (row < MID_INDEX + height && row >= height) {
-            validPaths.add(new Direction(1, 0, 1));
+            VALID_PATHS.add(new Direction(1, 0, 1));
         }
+        // upward diagonal
         if (row > MID_INDEX - height && row < PLANE_SIZE - height) {
-            validPaths.add(new Direction(-1, 0, 1));
+            VALID_PATHS.add(new Direction(-1, 0, 1));
         }
-        if (col < MID_INDEX + height && row < MID_INDEX + height && col >= height && row >= height) {
-            validPaths.add(new Direction(1, 1, 1));
+        // downward + rightward diagonal
+        if (col < MID_INDEX + height && row < MID_INDEX + height &&
+                col >= height && row >= height) {
+            VALID_PATHS.add(new Direction(1, 1, 1));
         }
-        if ((col > MID_INDEX - height && row < MID_INDEX + height) && col < PLANE_SIZE - height && row >= height) {
-            validPaths.add(new Direction(1, -1, 1));
+        // downward + leftward diagonal
+        if (col > MID_INDEX - height && row < MID_INDEX + height &&
+                col < PLANE_SIZE - height && row >= height) {
+            VALID_PATHS.add(new Direction(1, -1, 1));
         }
-        if ((col > MID_INDEX - height && row > MID_INDEX - height) && (col < PLANE_SIZE - height && row < PLANE_SIZE - height)) {
-            validPaths.add(new Direction(-1, -1, 1));
+        // upward + rightward diagonal
+        if ((col > MID_INDEX - height && row > MID_INDEX - height) &&
+                (col < PLANE_SIZE - height && row < PLANE_SIZE - height)) {
+            VALID_PATHS.add(new Direction(-1, -1, 1));
         }
-        if (col < MID_INDEX + height && row > MID_INDEX - height && col >= height && row < PLANE_SIZE - height) {
-            validPaths.add(new Direction(-1, 1, 1));
+        // upward + leftward diagonal
+        if (col < MID_INDEX + height && row > MID_INDEX - height &&
+                col >= height && row < PLANE_SIZE - height) {
+            VALID_PATHS.add(new Direction(-1, 1, 1));
         }
+        // vertical only added on max height level
         if (height == HEIGHT) {
-            validPaths.add(new Direction(0, 0, -1));
+            VALID_PATHS.add(new Direction(0, 0, -1));
         }
     }
 
+    /**
+     * Determines if two Search Tables are
+     * equivalent. Order is irrelevant.
+     *
+     * @param other other search table
+     * @return true if other and this search table
+     * contain the same set of directions.
+     */
     public boolean equals(Object other) {
         if (!(other instanceof SearchLookUpTable)) {
             return false;
         }
-        return ((SearchLookUpTable) other).validPaths.equals(this.validPaths);
+        return ((SearchLookUpTable) other).VALID_PATHS.equals(this.VALID_PATHS);
     }
 
+    /**
+     * Determines if a direction is within this
+     * search table.
+     *
+     * @param set direction obj being searched
+     *            in this list
+     * @return true if set is found in the
+     * search table
+     */
     public boolean contains(Object set) {
         if (!(set instanceof Direction)) {
             return false;
         }
-        for (Direction s : validPaths) {
+        for (Direction s : VALID_PATHS) {
             if (s.equals(set)) {
                 return true;
             }
@@ -70,27 +118,41 @@ public class SearchLookUpTable implements CONNECT_CONSTANTS{
         return false;
     }
 
+    /**
+     * Adds a direction to the search table.
+     * Do not allow same elements in table.
+     *
+     * @param set direction being added, set != null.
+     * @return true if the element can be added.
+     */
     public boolean add(Direction set) {
-        boolean add = !validPaths.contains(set);
+        if (set == null) {
+            throw new IllegalArgumentException("Violation: adding null elem to search table.");
+        }
+        boolean add = !VALID_PATHS.contains(set);
         if (add) {
-            validPaths.add(set);
+            VALID_PATHS.add(set);
         }
         return add;
     }
 
-    public boolean combine(SearchLookUpTable other) {
-        int origSize = validPaths.size();
-        for (Direction i : other.validPaths) {
-            this.add(i);
-        }
-        return origSize != validPaths.size();
-    }
-
+    /**
+     * Iterator used to allow the traversal of
+     * directions within the search table.
+     *
+     * @return a iterator used to access elements
+     */
     public Iterator<Direction> iterator() {
-        return validPaths.iterator();
+        return VALID_PATHS.iterator();
     }
 
+    /**
+     * Prints out the direction sets for a point.
+     *
+     * @return a string revealing the contents of
+     * the search table.
+     */
     public String toString() {
-        return validPaths.toString();
+        return VALID_PATHS.toString();
     }
 }
