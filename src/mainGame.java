466f5390e5b690e1ed46a27a44641b84fb11c262
Misbah <misb2001@gmail.com>
2021-06-09 13:26:18 -0500

    - Added Javafx library to the project

--- src/mainGame.java
+++ src/mainGame.java
@@ -1,5 +1,23 @@
-public class mainGame {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class mainGame extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.HONEYDEW);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
+        launch(args);
         game();
     }
 
